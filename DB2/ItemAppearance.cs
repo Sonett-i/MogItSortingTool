0279bc713289afbf03461a92a911ddac5423fa38
sonett-i <sam@sonetti.net>
2024-02-29 19:52:21 +1030

    data structs

---
+++ DB2/ItemAppearance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MogItSortingTool.DB2
+{
+	internal class ItemAppearance
+	{
+	}
+}
