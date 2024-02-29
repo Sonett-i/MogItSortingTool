6916cf131ed8da5ad7de0e9aa377d7242c4d5c0e
sonett-i <sam@sonetti.net>
2024-02-29 15:32:29 +1030

    Add project files.

---
+++ Libs/iConsole.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Libs.ConsoleUtils
+{
+	public abstract class iConsole
+	{
+
+		public static void Print(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+
+		public static void Print(params object[] args)
+		{
+			string output = "";
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				output += args[i].ToString();
+			}
+
+			Print(output);
+		}
+
+		public static string PrintArray(int[] array)
+		{
+			string output = "";
+			for (int i = 0; i < array.Length; i++)
+			{
+				output += array[i].ToString() + ", ";
+				if (i % 8 == 0)
+				{
+					output += "\n";
+				}
+			}
+			return output;
+		}
+
+		public static void Clear()
+		{
+			Console.Clear();
+		}
+		public static string Prompt(string message)
+		{
+			iConsole.Print(message);
+			string? input = "";
+
+			input = Console.ReadLine();
+
+			if (input == null)
+				input = "";
+			return input;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Libs.ConsoleUtils;
+
+namespace MogItSortingTool
+{
+	class Program
+	{
+		public static int Main(string[] args)
+		{
+			iConsole.Print("abc", "deg");
+			
+			return (0);
+		}
+	}
+}
